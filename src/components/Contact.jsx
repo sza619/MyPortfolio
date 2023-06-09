eab01927950121a18a6686a7cfbd29db0575c0f1
sza619 <szahere619@gmail.com>
2023-06-09 23:10:31 +0530

    Send button working

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -104,7 +104,7 @@ const Contact = () => {
 
           <button
             type="submit"
-            className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
+            className="bg-tertiary hover:bg-sky-700 py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
           >
             {loading ? "Sending..." : "Send"}
           </button>
